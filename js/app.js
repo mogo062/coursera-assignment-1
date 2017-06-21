e254d0c98f0193f10dc090e795d907c161482c8a
MONGI GUESMI <03989@biat.int>
2017-06-21 15:51:16 +0100

    First Assignment

--- js/app.js
+++ js/app.js
@@ -10,18 +10,22 @@ function LunchCheckController($scope,$filter,$injector){
   $scope.lunchMenu="";
   $scope.menuLength=0;
   $scope.checkValidItems=function(){
+    /*
+    * Check Empty field
+    */
    if($scope.lunchMenu === "" ){
       $scope.msg="Please enter data first";
       $scope.msgClass="has-error";
+      $scope.menuLength=0;
       return;
     }
-
+    /** convert text to array : split content field by ',' */
    var menu = $scope.lunchMenu.split(",");
    $scope.menuLength=menu.length;
+
+   /** Detect empty Item : if exist decrease menuLength */
    detectEmptyItem(menu);
-/*   if(detectEmptyItem(menu)){
-     return;
-   }*/
+   /** Check menu length */
    if($scope.menuLength <= 3){
       $scope.msg="Enjoy !";
     }else{
@@ -29,20 +33,17 @@ function LunchCheckController($scope,$filter,$injector){
     }
     $scope.msgClass="has-success";
   };
-
+  /** function : detectEmptyItem
+   * input : array (menu)
+   * desc: validate input array item(one by one) by using regular expression
+   */
  var detectEmptyItem=function(items){
     var emptyRegex = /^ *$/;
     for(var i = 0 ; i< items.length ;i++){
       if(emptyRegex.test(items[i])){
         $scope.menuLength--;
-        //    $scope.msg="Empty item detected in position "+i;
-        //  $scope.msgClass="has-warning";
       }
     }
-    if($scope.menuLength != items.length){
-      return true;
-    }
-    return false;
   };
 
 };
